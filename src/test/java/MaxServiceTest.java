be23ccb4532f90d2be2f8df1e31f1955afb65c90
AnF <anfwork007@gmail.com>
2024-04-02 22:50:51 +0300

    gitignore modified3

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -7,10 +7,23 @@ public class MaxServiceTest {
     public void shouldFindMax() {
         MaxService service = new MaxService();
 
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxB() {
+        MaxService service = new MaxService();
+
         int a = 3;
         int b = 5;
 
-        int expected = a;
+        int expected = b;
         int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
